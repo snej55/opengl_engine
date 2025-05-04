f5ed5ce9f64f6ece2abbe14213eec34b097ccd15
Jens Kromdijk <jenskromdijk@gmail.com>
2025-05-04 19:48:07 +0100

    Add IOHandler

--- src/iohandler.h
+++ src/iohandler.h
@@ -1,4 +1,22 @@
 #ifndef IO_HANDLER_H
 #define IO_HANDLER_H
 
+#include "engine.h"
+
+class IOHandler : public EngineObject
+{
+public:
+    IOHandler(EngineObject* window);
+
+    // check whether we need to quit
+    virtual void update();
+
+    bool getPressed(int key) const;
+    [[nodiscard]] bool getQuit() const {return m_quit;}
+
+private:
+    bool m_quit{false};
+    EngineObject* m_window{nullptr};
+};
+
 #endif
