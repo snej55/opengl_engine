155e4615f3835cba741bbca3ab1d1f0de47f059d
snej55 <jenskromdijk@gmail.com>
2025-05-04 16:45:37 +0100

    Add window initialization

--- src/window.cpp
+++ src/window.cpp
@@ -1,5 +1,7 @@
 #include "window.h"
 
+#include <iostream>
+
 Window::Window(EngineObject* parent)
  : EngineObject{parent}
 {
@@ -8,4 +10,18 @@ Window::Window(EngineObject* parent)
 bool Window::init(const int width, const int height, const char* title)
 {
     m_window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+
+    // validate window
+    if (m_window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window!" << std::endl;
+        return false;
+    }
+    glfwMakeContextCurrent(m_window);
+
+    setWidth(width);
+    setHeight(height);
+    setTitle(title);
+
+    return true;
 }
