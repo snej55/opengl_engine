94be66d99d5151aad72c9bd6994105fff58714c8
snej55 <jenskromdijk@gmail.com>
2025-05-04 15:52:47 +0100

    Work on window setup

---
+++ src/window.cpp
@@ -0,0 +1,11 @@
+#include "window.h"
+
+Window::Window(EngineObject* parent)
+ : EngineObject{parent}
+{
+}
+
+bool Window::init(const int width, const int height)
+{
+    
+}
--- src/window.h
+++ src/window.h
@@ -3,10 +3,13 @@
 
 #include "engine.h"
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 class Window : public EngineObject
 {
 public:
-    Window();
+    Window(EngineObject* parent);
 
     bool init(const int width, const int height);
     void free();
@@ -14,6 +17,8 @@ public:
 private:
     int m_width{0};
     int m_height{0};
+
+    GLFWwindow* m_Window{nullptr};
 };
 
 #endif
