2a8eac595e22df30baa68df9654a15ba54dbada4
Jens Kromdijk <jenskromdijk@gmail.com>
2025-05-06 14:39:48 +0100

    Add shader compilation

--- src/shader.cpp
+++ src/shader.cpp
@@ -33,6 +33,7 @@ void Shader::loadFromFile(const char* fragPath, const char* vertPath)
     vertFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
     fragFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
     
+    // load shader source from file
     try
     {
         vertFile.open(vertPath);
@@ -52,4 +53,54 @@ void Shader::loadFromFile(const char* fragPath, const char* vertPath)
         std::cout << "SHADER::LOAD_FROM_FILE::ERROR: Could not read source files: {vert: `" << vertPath << "`, frag: `" << fragPath << "`}" << std::endl;
         return;
     }
+
+    const char* vShaderCode {vertCode.c_str()};
+    const char* fShaderCode {fragCode.c_str()};
+
+    // compile shaders
+    unsigned int vertex, fragment;
+    int success;
+    char infoLog[512]; // for errors
+
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vShaderCode, nullptr);
+    glCompileShader(vertex);
+    glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertex, 512, nullptr, infoLog);
+        std::cout << "SHADER::LOAD_FROM_FILE::ERROR: Vertex shader compilation failed." << std::endl << infoLog;
+    }
+
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fShaderCode, nullptr);
+    glCompileShader(fragment);
+    glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragment, 512, nullptr, infoLog);
+        std::cout << "SHADER::LOAD_FROM_FILE::ERROR: Fragment shader compilation failed." << std::endl << infoLog;
+    }
+
+    // actually create the program
+    m_ID = glCreateProgram();
+    glAttachShader(m_ID, vertex);
+    glAttachShader(m_ID, fragment);
+    glLinkProgram(m_ID);
+    // get linking errors
+    glGetProgramiv(m_ID, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(m_ID, 512, nullptr, infoLog);
+        std::cout << "SHADER::LOAD_FROM_FILE::ERROR: Shader linking failed." << std::endl << infoLog;
+    }
+
+    // no longer needed
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+}
+
+unsigned int Shader::getID() const
+{
+    return 0;
 }
--- src/shader.h
+++ src/shader.h
@@ -1,6 +1,7 @@
 #ifndef SHADER_H
 #define SHADER_H
 
+#include <glad/glad.h>
 #include "engine.h"
 
 #include <GLFW/glfw3.h>
@@ -15,7 +16,7 @@ public:
 
     virtual void loadFromFile(const char* fragPath, const char* vertPath);
 
-    virtual void loadDefault();
+    // virtual void loadDefault();
 
     [[nodiscard]] unsigned int getID() const;
 
