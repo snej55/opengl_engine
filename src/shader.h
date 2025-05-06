a1a9b87060ebcd4d14a12f0713fcd7e94c3b5258
Jens Kromdijk <jenskromdijk@gmail.com>
2025-05-06 07:16:53 +0100

    Start work on shader object

---
+++ src/shader.h
@@ -0,0 +1,26 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include "engine.h"
+
+#include <GLFW/glfw3.h>
+
+class Shader : public EngineObject
+{
+public:
+    Shader(EngineObject* parent);
+    virtual ~Shader();
+
+    virtual void free();
+
+    virtual void loadFromFile(const char* fragPath, const char* vertPath);
+
+    virtual void loadDefault();
+
+    [[nodiscard]] unsigned int getID() const;
+
+protected:
+    unsigned int m_ID;
+};
+
+#endif
