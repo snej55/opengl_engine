c7b3658bafe780334f8aae73b1dcab364536f09b
snej55 <jenskromdijk@gmail.com>
2025-05-06 16:57:30 +0100

    Update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+#include "src/app.h"
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "Testing, testing" << std::endl;
+    App app{};
+    if (!app.init(640, 480, "OpenGL window"))
+    {
+        std::cout << "Failed to initialize!" << std::endl;
+        return 1;
+    }
+    std::cout << "Initialized!\n";
     return 0;
 }
