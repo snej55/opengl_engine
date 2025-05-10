55a3a2574a6ab64f1e76ec8cb52431c6c3687188
snej55 <jenskromdijk@gmail.com>
2025-05-10 11:14:43 +0100

    Define shader uniform setters

--- src/shader.h
+++ src/shader.h
@@ -23,24 +23,24 @@ public:
     [[nodiscard]] unsigned int getID() const;
 
     // shader uniforms
-    void setBool(const std::string_view name, const bool value) const;
-    void setInt(const std::string_view name, const int value) const;
-    void setFloat(const std::string_view name, const float value) const;
+    void setBool(const std::string& name, const bool value) const;
+    void setInt(const std::string& name, const int value) const;
+    void setFloat(const std::string& name, const float value) const;
 
     // vectorz
-    void setVec2(const std::string_view name, const glm::vec2& value) const;
-    void setVec2(const std::string_view name, const float x, const float y) const;
+    void setVec2(const std::string& name, const glm::vec2& value) const;
+    void setVec2(const std::string& name, const float x, const float y) const;
 
-    void setVec3(const std::string_view name, const glm::vec3& value) const;
-    void setVec3(const std::string_view name, const float x, const float y, const float z) const;
+    void setVec3(const std::string& name, const glm::vec3& value) const;
+    void setVec3(const std::string& name, const float x, const float y, const float z) const;
 
-    void setVec4(const std::string_view name, const glm::vec4& value) const;
-    void setVec4(const std::string_view name, const float x, const float y, const float z, const float w) const;
+    void setVec4(const std::string& name, const glm::vec4& value) const;
+    void setVec4(const std::string& name, const float x, const float y, const float z, const float w) const;
 
     // matrices
-    void setMat2(const std::string_view name, const glm::mat2& value) const;
-    void setMat3(const std::string_view name, const glm::mat3& value) const;
-    void setMat4(const std::string_view name, const glm::mat4& value) const;
+    void setMat2(const std::string& name, const glm::mat2& value) const;
+    void setMat3(const std::string& name, const glm::mat3& value) const;
+    void setMat4(const std::string& name, const glm::mat4& value) const;
 
 protected:
     unsigned int m_ID;
