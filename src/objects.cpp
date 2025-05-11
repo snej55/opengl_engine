7e4134cb60c96cbc952b43af1fcee4b06597d8a7
snej55 <jenskromdijk@gmail.com>
2025-05-11 13:56:40 +0100

    Fix vertex buffer load issue

--- src/objects.cpp
+++ src/objects.cpp
@@ -23,7 +23,7 @@ void Object::init(const std::vector<float>& vertices)
 
     // buffer vertices
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(vertices.size()), vertices.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(vertices.size() * sizeof(vertices[0])), vertices.data(), GL_STATIC_DRAW);
 
     // load vertex array
     glBindVertexArray(vao);
