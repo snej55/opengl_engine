a18ac164694eac60c1b24fe6ac811183f7931767
snej55 <jenskromdijk@gmail.com>
2025-06-08 17:45:46 +0100

    Add clock

---
+++ src/clock.cpp
@@ -0,0 +1,30 @@
+#include "clock.hpp"
+
+Clock::Clock(EngineObject* engine)
+ : EngineObject{"Clock", engine}
+{
+    update();
+}
+
+void Clock::update()
+{
+    // recalculate delta time
+    const float time{static_cast<float>(glfwGetTime())};
+    m_deltaTime = time - m_lastTime;
+    m_lastTime = time;
+
+    // update time
+    m_time = static_cast<float>(glfwGetTime());
+}
+
+float Clock::getDeltaTime() const
+{
+    return m_deltaTime;
+}
+
+float Clock::getTime()
+{
+    // update time
+    m_time = static_cast<float>(glfwGetTime());
+    return m_time;
+}
