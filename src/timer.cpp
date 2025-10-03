ff9fd92a4544ec3bb38a5f50f68593b85fe0c90a
snej55 <jenskromdijk@gmail.com>
2025-10-03 10:50:48 +0100

    Add time class

---
+++ src/timer.cpp
@@ -0,0 +1,34 @@
+#include "timer.hpp"
+
+// for glfwGetTime()
+#include <GLFW/glfw3.h>
+
+Timer::Timer()
+{
+    // initialize start time and last time
+    reset();
+}
+
+// update last time
+void Timer::update()
+{
+    m_lastTime = static_cast<float>(glfwGetTime());
+}
+
+// reset start time and last time
+void Timer::reset()
+{
+    m_startTime = static_cast<float>(glfwGetTime());
+    m_lastTime = m_startTime;
+}
+
+// get time as int
+int Timer::getTime() const
+{
+    return static_cast<int>(m_lastTime - m_startTime);
+}
+
+float Timer::getTimeF() const
+{
+    return m_lastTime - m_startTime;
+}
---
+++ src/timer.hpp
@@ -0,0 +1,32 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+class Timer
+{
+public:
+    Timer();
+    ~Timer() = default;
+
+    // update time delta
+    void update();
+    // reset start time
+    void reset();
+
+    // gets the time in seconds as an int
+    [[nodiscard]] int getTime() const;
+    // gets the time in seconds as a float
+    [[nodiscard]] float getTimeF() const;
+
+    // get the start time of the timer
+    [[nodiscard]] float getRawStartTime() const {return m_startTime;}
+    // get the last time of the timer
+    [[nodiscard]] float getRawLastTime() const {return m_lastTime;}
+
+private:
+    // start time (time since glfw initialized in seconds)
+    float m_startTime{};
+    // last time recorded
+    float m_lastTime{};
+};
+
+#endif
